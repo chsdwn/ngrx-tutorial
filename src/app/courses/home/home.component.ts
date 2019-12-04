cb0683438ff941b017cebdde58f145ea4c02e71d
chsdwn <hulusikafalier@gmail.com>
2019-12-04 11:34:11 +0300

    Initial commit

---
+++ src/app/courses/home/home.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { defaultDialogConfig } from '../shared/default-dialog-config';
+import { EditCourseDialogComponent } from '../edit-course-dialog/edit-course-dialog.component';
+
+import { CoursesHttpService } from '../services/courses-http.service';
+
+import { compareCourses, Course } from '../model/course';
+
+@Component({
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  promoTotal$: Observable<number>;
+  loading$: Observable<boolean>;
+  beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;
+
+  constructor(
+    private dialog: MatDialog,
+    private coursesHttpService: CoursesHttpService) {
+  }
+
+  ngOnInit() {
+    this.reload();
+  }
+
+  reload() {
+    const courses$ = this.coursesHttpService.findAllCourses()
+      .pipe(
+        map(courses => courses.sort(compareCourses)),
+        shareReplay()
+      );
+    this.loading$ = courses$.pipe(map(courses => !!courses));
+
+    this.beginnerCourses$ = courses$
+      .pipe(
+        map(courses => courses.filter(course => course.category === 'BEGINNER'))
+      );
+
+    this.advancedCourses$ = courses$
+      .pipe(
+        map(courses => courses.filter(course => course.category === 'ADVANCED'))
+      );
+
+    this.promoTotal$ = courses$
+      .pipe(
+        map(courses => courses.filter(course => course.promo).length)
+      );
+  }
+
+  onAddCourse() {
+    const dialogConfig = defaultDialogConfig();
+    dialogConfig.data = {
+      dialogTitle: 'Create Course',
+      mode: 'create'
+    };
+    this.dialog.open(EditCourseDialogComponent, dialogConfig);
+  }
+}
